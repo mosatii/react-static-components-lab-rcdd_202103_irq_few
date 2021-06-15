6d54c1eff6734603f85ff71255f606e2e90b48d7
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-06-15 11:09:52 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import GraceHopperQuoteComponent from './src/GraceHopperQuoteComponent'
+import CatComponent from './src/CatComponent'
 class App extends Component {
 	render() {
 		// your code in the return statement below!
